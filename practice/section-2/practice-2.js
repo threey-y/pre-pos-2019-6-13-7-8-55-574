0df2a86bc02d4a59386048eba569e53f3f47a6e8
threey-y <threey.y@foxmail.com>
2019-08-19 22:11:01 +0800

    Section 2-3

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,29 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var newArr = [];
+  var map = new Map();
+  var v,c;
+  for (var i=0;i<collection.length;i++){
+    if(collection[i].length==1){
+      if (map.get(collection[i])==null){
+        map.set(collection[i],1);
+      }else{
+        map.set(collection[i],map.get(collection[i])+1);
+      }
+    }else{
+      v=collection[i][0];
+      c=Number(collection[i][2]);
+      if (map.get(v)==null){
+        map.set(v,c);
+      }else{
+        map.set(v,map.get(v)+c);
+      }
+    }
+    
+  }
+  map.forEach(function(key,value){
+    newArr.push({key:value,count:key})
+  });
+  return newArr; 
 }
--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,5 +1,35 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var newArr = [];
+  var map = new Map();
+  for (var i=0;i<collectionA.length;i++){
+    if (map.get(collectionA[i])==null){
+      map.set(collectionA[i],1);
+    }else{
+      map.set(collectionA[i],map.get(collectionA[i])+1);
+    }
+  }
+
+  map.forEach(function(key,value){
+    newArr.push({key:value,count:key})
+  });
+  console.log(newArr);
+
+
+  var  collectionB = objectB.value;
+  for (var i = 0;i < newArr.length;i++)
+  {
+
+    for (var j = 0;j<collectionB.length;j++)
+    {
+      if (newArr[i].key == collectionB[j])
+      {
+        var temp = newArr[i].count % 3;
+        var  a = (newArr[i].count - temp) / 3;
+        newArr[i].count = newArr[i].count - a;
+      }
+    }
+  }
+  return newArr;
 }
